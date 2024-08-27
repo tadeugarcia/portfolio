528e7b4e5b347247b86a9792d753b542c9c4b7eb
Tadeu Garcia <tadeugarcia.92@gmail.com>
2024-08-27 12:44:52 -0300

    migração do index para o nextjs

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -3,111 +3,86 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+      <section className="h-screen">
+        <p>Tadeu Garcia</p>
+        <h1>Senior Product Designer based in Rio de Janeiro</h1>
+        <p>
+          Experienced designer specializing in creating digital products for e-commerce, marketplaces, 
+          billing and payment systems, as well as back-office applications. Skilled in developing web components 
+          for desktop and mobile platforms, with expertise in Material Design for app interfaces.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+        <p>Brazilian, obtaining Spanish citizenship by end-2024.</p>
+      </section>
+
+      <section className="h-screen">
+        <h2>Case studies</h2>
+        <p>Relevant experience from recent years</p>
+
+        <a href="case-1" className="video-intro">
+          <video>
+            <source src="@public/posgraduate/video.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </a>
+
+        <h3>Premium Posgraduate Redesign</h3>
+        
+        <p>In this redesign, the navigation flow and the entire course purchase process were completely renewed.
+        
+        <a href="case-1">View case</a></p>
+        
+        <a href="case-2" className="video-intro">
+          <video>
+            <source src="@public/re-enrollment/video.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </a>
+        
+        <h3>Payment of outstanding fees</h3>
+        
+        <p>Streamlining the process for students to pay outstanding tuition fees before re-enrolling for&nbsp;the&nbsp;next&nbsp;term.</p>
+        
+        <a href="case-2">View case</a>
+      </section>
+      
+      <section className="h-screen">
+        <h2>Acessibility and code </h2>
+        
+        <p>What sets me apart is a long-standing interest in understanding the background workings of interfaces.</p>
+        <p>In my previous role, one of the directors there, who was also a developer and a blind person, taught me 
+        so much about <strong>accessibility: from best practices to using screen readers</strong>, it was an 
+        incredible learning with him.</p>
+      </section>
+
+      <section className="h-screen">
+        <h2>More about me </h2>
+        
+        <ul>
+          <li>Started designing in 2007 with an online forum signature contest.</li>
+          <li>AI enthusiast, I utilize Artificial Intelligence daily to assist me with work and personal tasks.</li>
+          <li>To proceed with the hipster designer checklist, I'm vegetarian.</li>
+          <li>My current hobbies are swimming, yoga, and meditation.</li>
+        </ul>
+      </section>
+
+      <section className="h-screen">
+        <h2>You can find me over here </h2>
+
+        <div className="social">
+          <a href="https://www.linkedin.com/in/garciatadeu/" target="_blank">
+            <span className="material-symbols-outlined">arrow_outward</span> LinkedIn
+          </a>
+          <a href="assets/resume/Tadeu_Garcia_Resume.pdf" target="_blank">
+            <span className="material-symbols-outlined">description</span> Resume
+          </a>
+          <a href="mailto:tadeugarcia.92@gmail.com">
+            <span className="material-symbols-outlined">mail</span> tadeugarcia.92@gmail.com
+          </a>
+          <a href="tel:+55 21 97997 6655">
+            <span className="material-symbols-outlined">phone</span> +55 21 97997 6655
           </a>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
     </main>
   );
 }
