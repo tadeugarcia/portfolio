3b33f16214a40634ac1aa92038daf5152de73204
tadeugarcia <tadeugarcia.92@gmail.com>
2024-08-30 17:38:09 -0300

    instalação e configuração do @next/mdx package

--- src/app/reenrollment/page.tsx
+++ src/app/reenrollment/page.tsx
@@ -1,22 +1,5 @@
-export default function ReenrollmentPage() {
-	return (
-    <main>
-		<div className="nav-start">
-			<a href="/">
-				<span className="material-symbols-outlined">arrow_left_alt</span> All work
-			</a>
-		</div>
-
-		<div className="video-intro">
-			<video loop muted>
-				<source src="video.mp4" type="video/mp4"/>
-				Your browser does not support the video tag.
-			</video>
-		</div>
-
-		<main id="content"></main>
-		<h2>Thank you for your time</h2>
-		<p>Let's return to view  <a href="/">all work <span className="material-symbols-outlined">arrow_right_alt</span></a></p>
-    </main>
-  );
+import Reenrollment from './reenrollment.mdx'
+ 
+export default function Page() {
+  return <Reenrollment />
 }
