361985156952dba7d74312d0c2437bf05d32f69d
tadeugarcia <tadeugarcia.92@gmail.com>
2024-09-04 14:41:24 -0300

    GTM como primeiro import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-import type { Metadata } from "next";
 import { GoogleTagManager } from '@next/third-parties/google';
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
