c235e11f83f0573731b9837e7bb35e1f535ed14b
tadeugarcia <tadeugarcia.92@gmail.com>
2024-09-07 12:11:39 -0300

    instalação do framer-motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import * as motion from "framer-motion/client";
 import Image from "next/image";
 import reenrollment from '/public/re-enrollment/case.png';
 import posgraduate from '/public/posgraduate/case.png';
@@ -7,32 +8,106 @@ export default function Home() {
     <main>
       <section className="space-y-7">
         <div className="text-center">
-          <p className="title">Tadeu Garcia</p>
-          <h1 className="font-light text-zinc-300"><span className="font-bold">Senior Product Designer</span> based in Rio de Janeiro🌴</h1>
+          <motion.p          
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, y: [20, 0] }}
+            transition={{ duration: 0.4, ease: "easeOut" }}
+            className="title">
+            Tadeu Garcia
+          </motion.p>
+          <h1 className="text-zinc-300">
+            <motion.span            
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1, y: [20, 0]}}
+              transition={{ delay: 0.4, duration: 0.4, ease: "easeOut" }}
+              className="font-bold">
+              Senior Product Designer&nbsp;
+            </motion.span>           
+            <motion.span          
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1, x: [20, 0] }}
+              transition={{ delay: 0.8, duration: 0.4, ease: "easeOut" }}
+              className="font-light">
+              based in Rio de Janeiro🌴
+            </motion.span>
+          </h1>
         </div>
         <div className="flex space-x-2">
-          <span className="chip">financial services</span>
-          <span className="chip">payment systems</span>
-          <span className="chip">e-commerce</span>
-          <span className="chip">b2b and b2c saas</span>
-          <span className="chip">front-end</span>
+          <motion.span 
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, y: [20, 0] }}
+            transition={{ delay: 1 + .1, duration: 0.4, ease: "easeOut" }}
+            className="chip">
+            financial services
+          </motion.span>
+          <motion.span  
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, y: [20, 0] }}
+            transition={{ delay: 1 + .2, duration: 0.4, ease: "easeOut" }}
+            className="chip">
+            payment systems
+          </motion.span>
+          <motion.span  
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, y: [20, 0] }}
+            transition={{ delay: 1 + .3, duration: 0.4, ease: "easeOut" }}
+            className="chip">
+            e-commerce
+          </motion.span>
+          <motion.span  
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, y: [20, 0] }}
+            transition={{ delay: 1 + .4, duration: 0.4, ease: "easeOut" }}
+            className="chip">
+            b2b and b2c saas
+          </motion.span>
+          <motion.span  
+            initial={{ opacity: 0}}
+            animate={{ opacity: 1, y: [20, 0]}}
+            transition={{ delay: 1 + .5, duration: 0.4, ease: "easeOut" }}
+            className="chip">
+            front-end
+          </motion.span>
         </div>
 
-        <video width="720" height="480" controls controlsList="nodownload" disablePictureInPicture preload="auto" 
+        <motion.video
+          width="720" height="480" 
+          controls controlsList="nodownload" 
+          disablePictureInPicture preload="auto"
+          animate={{ opacity: [0, 1], scale: [.9, 1]}}
+          transition={{ delay: 1.7, duration: 0.4 }}
           className="rounded-2xl shadow-2xl shadow-zinc-500/40">
           <source src="intro.mp4" type="video/mp4" />
           Your browser does not support the video tag.
-        </video>
+        </motion.video>
       </section>
 
       <section id="case-studies" className="space-y-8">
           <div className="text-white text-center">
-            <h2 className="title">Case studies</h2>
-            <p>Relevant experience from recent years</p>
+            <motion.h2 
+              viewport={{ once: true }}
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1, y: [20, 0] }}
+              transition={{ duration: 0.4, ease: "easeOut" }}
+              className="title">
+              Case studies
+            </motion.h2>
+            <motion.p            
+              viewport={{ once: true }}
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1, y: [20, 0] }}
+              transition={{ delay: 0.2, duration: 0.4, ease: "easeOut" }}>
+              Relevant experience from recent years
+            </motion.p>
           </div>
 
           <div className="flex flex-row space-x-5">
-            <a className="case-card hover:border-purple-500 hover:shadow-purple-500/40" href="/re-enrollment">
+            <motion.a
+              viewport={{ once: true }}
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1, y: [20, 0] }}
+              transition={{ delay: 0.5, duration: 0.4, ease: "easeOut" }} 
+              className="case-card hover:border-purple-500 hover:shadow-purple-500/40" href="/re-enrollment">
               <ul className="flex space-x-2">
                 <li className="chip">usability testing</li>
                 <li className="chip">user behavior analysis</li>
@@ -43,9 +118,14 @@ export default function Home() {
                 <h3 className="case-title">Payment of outstanding fees</h3>
                 <p>How I designed the re-enrollment flow, automating financial processes and <span className="underline decoration-2 decoration-purple-500">boosting debt clearance and re-enrollments.</span></p>
               </div>
-            </a>
+            </motion.a>
 
-            <a className="case-card hover:border-yellow-500 hover:shadow-yellow-500/40" href="/posgraduate">
+            <motion.a
+              viewport={{ once: true }}
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1, y: [20, 0] }}
+              transition={{ delay: 0.8, duration: 0.4, ease: "easeOut" }} 
+              className="case-card hover:border-yellow-500 hover:shadow-yellow-500/40" href="/posgraduate">
               <ul className="flex space-x-2">
                 <li className="chip">desk research</li>
                 <li className="chip">usability testing</li>
@@ -57,40 +137,77 @@ export default function Home() {
                 <h3 className="case-title">Premium posgraduate redesign</h3>
                 <p>I redesigned the postgraduate course purchase flow with a premium UI, <span className="underline decoration-2 decoration-yellow-500">achieving 57.14% faster navigation.</span></p>
               </div>
-            </a>
+            </motion.a>
           </div>        
       </section>
       
       <section id="about me" className="flex flex-row text-white">        
         <div className="basis-1/3 justify-center">
-          <h2 className="title mb-10">About me</h2>
+          <motion.h2 
+              viewport={{ once: true }}
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1, y: [20, 0] }}
+              transition={{ duration: 0.4, ease: "easeOut" }}
+              className="title mb-10">
+              About me
+          </motion.h2>
           <div className="flex flex-col space-y-4">
-            <a href="https://www.linkedin.com/in/garciatadeu/" target="_blank" className="btn-link">
+            <motion.a 
+              viewport={{ once: true }}
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1, y: [20, 0] }}
+              transition={{ delay: 0.2 + .1, duration: 0.4, ease: "easeOut" }}
+              href="https://www.linkedin.com/in/garciatadeu/" target="_blank" className="btn-link">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M247.54-268.46 220-296l403.23-404H251.54v-40h440v440h-40v-371.69l-404 403.23Z"/></svg>
               LinkedIn
-            </a>
-            <a href="https://github.com/tadeugarcia" target="_blank" className="btn-link">
+            </motion.a>
+            <motion.a 
+              viewport={{ once: true }}
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1, y: [20, 0] }}
+              transition={{ delay: 0.2 + .2, duration: 0.4, ease: "easeOut" }}
+              href="https://github.com/tadeugarcia" target="_blank" className="btn-link">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M247.54-268.46 220-296l403.23-404H251.54v-40h440v440h-40v-371.69l-404 403.23Z"/></svg>
               GitHub
-            </a>
-            <a href="Tadeu Garcia - Resume.pdf" target="_blank" className="btn-link">
+            </motion.a>
+            <motion.a 
+              viewport={{ once: true }}
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1, y: [20, 0] }}
+              transition={{ delay: 0.2 + .3, duration: 0.4, ease: "easeOut" }}
+              href="Tadeu Garcia - Resume.pdf" target="_blank" className="btn-link">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M247.54-268.46 220-296l403.23-404H251.54v-40h440v440h-40v-371.69l-404 403.23Z"/></svg>
               Resume
-            </a>
-            <a href="mailto:tadeugarcia.92@gmail.com" className="btn-link">
+            </motion.a>
+            <motion.a 
+              viewport={{ once: true }}
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1, y: [20, 0] }}
+              transition={{ delay: 0.2 + .3, duration: 0.4, ease: "easeOut" }}
+              href="mailto:tadeugarcia.92@gmail.com" className="btn-link">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M247.54-268.46 220-296l403.23-404H251.54v-40h440v440h-40v-371.69l-404 403.23Z"/></svg>
               tadeugarcia.92@gmail.com
-            </a>
-            <a href="tel:+55 21 97997 6655" className="btn-link">
+            </motion.a>
+            <motion.a 
+              viewport={{ once: true }}
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1, y: [20, 0] }}
+              transition={{ delay: 0.2 + .4, duration: 0.4, ease: "easeOut" }}
+              href="tel:+55 21 97997 6655" className="btn-link">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M247.54-268.46 220-296l403.23-404H251.54v-40h440v440h-40v-371.69l-404 403.23Z"/></svg>
               +55 21 97997 6655
-            </a>
+            </motion.a>
           </div>
         </div>
 
         <div className="basis-2/3">
           <div className="grid grid-cols-2 space-x-5 text-white">
-            <div className="p-10 border rounded-2xl border-zinc-600">
+            <motion.div            
+              viewport={{ once: true }}
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1, y: [20, 0] }}
+              transition={{ delay: 1.5, duration: 0.4, ease: "easeOut" }}
+              className="p-10 border rounded-2xl border-zinc-600">
               <h2 className="font-bold text-2xl mb-5"> 
                 <span className="underline-blue">Code</span> and <span className="underline decoration-pink-500">acessibility</span>
               </h2>
@@ -101,9 +218,14 @@ export default function Home() {
                 <p>In my previous role, one of the directors there, who was also a developer and <strong className="underline decoration-2 decoration-pink-500">a blind person,</strong> taught me 
                 so much about accessibility: from <strong className="underline decoration-2 decoration-pink-500">best practices to using screen readers</strong>, it was an incredible learning with him.</p>
               </div>
-            </div>
+            </motion.div>
 
-            <div className="p-10 border rounded-2xl border-zinc-600">
+            <motion.div 
+              viewport={{ once: true }}
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1, y: [20, 0] }}
+              transition={{ delay: 1.8, duration: 0.4, ease: "easeOut" }}
+              className="p-10 border rounded-2xl border-zinc-600">
               <h2 className="font-bold text-2xl mb-5">More about Tadeu</h2>
               
               <ul className="font-light space-y-5">
@@ -113,7 +235,7 @@ export default function Home() {
                 <li>🏊‍♂️ My current hobbies are swimming, board sports, yoga and meditation.</li>
                 <li>🏆 Started designing in 2007 with an online forum signature contest.</li>
               </ul>
-            </div>
+            </motion.div>
           </div>
         </div>
       </section>
--- src/app/re-enrollment/layout.tsx
+++ src/app/re-enrollment/layout.tsx
@@ -1,3 +1,4 @@
+import * as motion from "framer-motion/client";
 import type { Metadata } from "next";
 import "../mdx.css";
 import "../re-enrollment.css";
@@ -9,23 +10,50 @@ export const metadata: Metadata = {
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
     return (
         <main>
-            <a href="/" className="fixed left-[40px] btn-link">
+            <motion.a         
+            viewport={{ once: true }}
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1, x: [-20, 0] }}
+            transition={{ delay: 1, duration: 0.4, ease: "easeOut" }}
+            href="/" className="fixed left-[40px] btn-link">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="m276.85-460 231.69 231.69L480-200 200-480l280-280 28.54 28.31L276.85-500H760v40H276.85Z"/></svg>
                 Portfolio overview
-            </a>
-            <a href="/posgraduate" className="fixed right-[40px] btn-link">
+            </motion.a>
+            <motion.a         
+            viewport={{ once: true }}
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1, x: [20, 0] }}
+            transition={{ delay: 1, duration: 0.4, ease: "easeOut" }}
+            href="/posgraduate" className="fixed right-[40px] btn-link">
                 Next case
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M683.15-460H200v-40h483.15L451.46-731.69 480-760l280 280-280 280-28.54-28.31L683.15-460Z"/></svg>
-            </a>
+            </motion.a>
             {children}
             <footer>
-                <h2>Thank you for your time</h2>               
+                <motion.h2
+                    viewport={{ once: true }}
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1, y: [20, 0] }}
+                    transition={{ delay: .2, duration: 0.4, ease: "easeOut" }}>
+                    Thank you for your time
+                </motion.h2>               
                 <p className="flex space-x-2">
-                    <span>Proceed to the</span>
-                    <a href="/posgraduate" className="btn-link">
+                    <motion.span
+                        viewport={{ once: true }}
+                        initial={{ opacity: 0 }}
+                        whileInView={{ opacity: 1, y: [20, 0] }}
+                        transition={{ delay: .5, duration: 0.4, ease: "easeOut" }}>
+                        Proceed to the
+                    </motion.span>
+                    <motion.a         
+                    viewport={{ once: true }}
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1, x: [-8, 0] }}
+                    transition={{ delay: 1, duration: 0.4, ease: "easeIn" }}
+                    href="/posgraduate" className="btn-link">
                         next case
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M683.15-460H200v-40h483.15L451.46-731.69 480-760l280 280-280 280-28.54-28.31L683.15-460Z"/></svg>
-                    </a>
+                    </motion.a>
                 </p>
             </footer>
         </main>
