1ce483c9965db3ce79565cc04594b9cc98f3f1f3
tadeugarcia <tadeugarcia.92@gmail.com>
2024-09-09 23:11:56 -0300

    animações em posgraduate

--- src/app/posgraduate/layout.tsx
+++ src/app/posgraduate/layout.tsx
@@ -1,3 +1,4 @@
+import * as motion from "framer-motion/client";
 import type { Metadata } from "next";
 import "../mdx.css";
 import "../posgraduate.css";
@@ -9,19 +10,40 @@ export const metadata: Metadata = {
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
     return (
         <main>
-            <a href="/re-enrollment" className="fixed left-[40px] btn-link">
+            <motion.a
+            viewport={{ once: true }}
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1, x: [-20, 0] }}
+            transition={{ delay: 1, duration: 0.4, ease: "easeOut" }}
+            href="/re-enrollment" className="fixed left-[40px] btn-link">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="m276.85-460 231.69 231.69L480-200 200-480l280-280 28.54 28.31L276.85-500H760v40H276.85Z"/></svg>
                 Previous case
-            </a>
+            </motion.a>
             {children}
             <footer>
-                <h2>Thank you for your time</h2>
+                <motion.h2
+                    viewport={{ once: true }}
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1, y: [20, 0] }}
+                    transition={{ delay: .2, duration: 0.4, ease: "easeOut" }}>
+                    Thank you for your time
+                </motion.h2>
                 <p className="flex space-x-2">
-                    <span>Return to</span>
-                    <a href="/" className="btn-link">
+                    <motion.span
+                        viewport={{ once: true }}
+                        initial={{ opacity: 0 }}
+                        whileInView={{ opacity: 1, y: [20, 0] }}
+                        transition={{ delay: .5, duration: 0.4, ease: "easeOut" }}>
+                        Return to
+                    </motion.span>
+                    <motion.a         
+                    viewport={{ once: true }}
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1, x: [-8, 0] }}
+                    transition={{ delay: 1, duration: 0.4, ease: "easeIn" }}
+                    href="/" className="btn-link">
                         portfolio overview
-                    </a>
-                    .
+                    </motion.a>
                 </p>
             </footer>
         </main>
