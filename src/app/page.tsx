4a4d311003bcf93afcaaa870c8a0034d5030e4e1
tadeugarcia <tadeugarcia.92@gmail.com>
2024-10-15 22:53:08 -0300

    novo hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { motion } from "framer-motion";
-import { useRef } from 'react';
 import Image from "next/image";
 import reenrollment1 from '/public/re-enrollment/new-screens-1.svg';
 import reenrollment2 from '/public/re-enrollment/new-screens-2.svg';
@@ -12,27 +11,20 @@ import posgraduate2 from '/public/posgraduate/new-screens-2.svg';
 import posgraduate3 from '/public/posgraduate/new-screens-3.svg';
 import posgraduate4 from '/public/posgraduate/new-screens-4.svg';
 import posgraduate5 from '/public/posgraduate/new-screens-5.svg';
-import { Linkedin02Icon, GithubIcon, GoogleDocIcon, PenTool03Icon, AnalyticsUpIcon, ViewIcon, SourceCodeCircleIcon, Beach02Icon, PlayIcon, CursorMagicSelection01Icon, UmbrellaIcon } from "hugeicons-react";
+import { Linkedin02Icon, GithubIcon, StackStarIcon, GoogleDocIcon, PenTool03Icon, AnalyticsUpIcon, ViewIcon, SourceCodeCircleIcon, Beach02Icon, PlayIcon, CursorMagicSelection01Icon, UmbrellaIcon } from "hugeicons-react";
 
 export default function Home() {
+  
   return (
     <main>      
-      <section>
+      <section className="min-h-[90vh]">
         <motion.div 
           viewport={{ once: true }}
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1, y: [20, 0] }}
           transition={{ delay: 0, duration: .4, ease: "easeOut" }}
-          className="container my-16 p-16 grid grid-cols-5 rounded-2xl backdrop-blur-sm bg-green-400/10 text-green-950">
-            <div className="col-span-3">            
-              <motion.h1
-                viewport={{ once: true }}
-                initial={{ opacity: 0 }}
-                whileInView={{ opacity: 1, y: [20, 0] }}
-                transition={{ delay: 0, duration: .4, ease: "easeOut" }}
-                className="text-lg font-bold">
-                Tadeu Garcia
-              </motion.h1>
+          className="container my-16 p-16 grid grid-cols-5 rounded-2xl backdrop-blur-sm bg-green-950/60 text-green-50">
+            <div className="col-span-3">   
               <motion.p          
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1, y: [20, 0] }}
@@ -40,63 +32,31 @@ export default function Home() {
                 className="title">
                 11 years in design.<br/>
                 Skilled at e-commerce, financial services and b2b/b2c SaaS
-              </motion.p>
-              <p>
-                <motion.span            
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1, y: [20, 0]}}
-                  transition={{ delay: .4, duration: .4, ease: "easeOut" }}
-                  className="font-bold">
-                  Senior Product Designer&nbsp;
-                </motion.span>
-                <motion.span          
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1, x: [20, 0] }}
-                  transition={{ delay: .8, duration: .4, ease: "easeOut" }}
-                  className="font-normal">
-                  based in Rio de Janeiro <Beach02Icon className="inline"/>
-                </motion.span>
-              </p>
+              </motion.p>    
               <motion.div
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1, y: [20, 0]}}
-                transition={{ delay: .8, duration: .4, ease: "easeOut" }}>
-                <a href="#first-case" className="button-primary mt-6 bg-green-600">
+                transition={{ delay: .8, duration: .4, ease: "easeOut" }}
+                className="mt-6 flex space-x-2">
+                <a href="#first-case" className="button-primary bg-green-600 border-green-600">
+                  <StackStarIcon/>
                   View case studies
+                </a>
+                <a href="Tadeu Garcia - Resume.pdf" target="_blank" className="button-outline hover:bg-white hover:text-green-950 hover:fill-green-950">
+                  <GoogleDocIcon/>
+                  View my resume
                 </a>
               </motion.div>
             </div>
 
-            <div className="col-span-2">            
-              <nav className="mx-auto mb-5 w-full inline-flex justify-end text-green-950">          
-                <motion.div
-                  viewport={{ once: true }}
-                  initial={{ opacity: 0 }}
-                  whileInView={{ opacity: 1, y: [20, 0] }}
-                  transition={{ delay: .8, duration: .4, ease: "easeOut" }}
-                  className="space-x-5">
-                  <a href="https://www.linkedin.com/in/garciatadeu/" target="_blank" className="navbar-link">
-                    <Linkedin02Icon/>
-                    LinkedIn
-                  </a>
-                  <a href="https://github.com/tadeugarcia" target="_blank" className="navbar-link">
-                    <GithubIcon/>
-                    GitHub
-                  </a>
-                  <a href="Tadeu Garcia - Resume.pdf" target="_blank" className="navbar-link">
-                    <GoogleDocIcon/>
-                    Resume
-                  </a>
-                </motion.div>
-              </nav>
-
+            <div className="col-span-2 flex flex-col space-y-4">
               <motion.div         
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1, y: [20, 0]}}
                 transition={{ delay: .4, duration: .4, ease: "easeOut" }}
                 className="relative">
                   <video
-                    className="w-full rounded-2xl shadow-2xl shadow-green-900/40"
+                    className="h-full rounded-2xl shadow-2xl shadow-green-900/40"
                     src="intro.mp4"
                     loop
                     autoPlay
@@ -108,26 +68,52 @@ export default function Home() {
                     Your browser does not support the video tag.
                   </video>
               </motion.div>
+
+              <div>
+                <motion.h1
+                  viewport={{ once: true }}
+                  initial={{ opacity: 0 }}
+                  whileInView={{ opacity: 1, y: [20, 0] }}
+                  transition={{ delay: 0, duration: .4, ease: "easeOut" }}
+                  className="text-lg font-bold">
+                  Tadeu Garcia
+                </motion.h1>
+                <p>
+                  <motion.span            
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1, y: [20, 0]}}
+                    transition={{ delay: .4, duration: .4, ease: "easeOut" }}
+                    className="font-light">
+                    Senior Product Designer&nbsp;
+                  </motion.span>
+                  <motion.span          
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1, x: [20, 0] }}
+                    transition={{ delay: .8, duration: .4, ease: "easeOut" }}
+                    className="font-semibold">
+                    based in Rio de Janeiro <Beach02Icon className="inline"/>
+                  </motion.span>
+                </p>
+              </div>              
             </div>
         </motion.div>
 
         <motion.div
           viewport={{ once: true }}
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: .08 }}
+          initial={{ opacity: 1 }}
           transition={{ delay: 0, duration: .4, ease: "easeOut" }}
-          className="-z-10 right-0 bottom-200rem absolute p-20 grid grid-cols-5 gap-16">
-            <Image src={reenrollment3} className="border rounded-2xl border-black/20" alt=""/>
-            <Image src={reenrollment5} className="border rounded-2xl border-black/20" alt=""/>
-            <Image src={reenrollment2} className="border rounded-2xl border-black/20" alt=""/>
-            <Image src={reenrollment1} className="border rounded-2xl border-black/20" alt=""/>
-            <Image src={reenrollment2} className="border rounded-2xl border-black/20" alt=""/>
+          className="-z-10 right-0 bottom-200rem absolute p-20 w-screen grid grid-cols-5 gap-16 bg-gradient-to-tr from-purple-100 via-emerald-100 to-lime-100">
+            <Image src={reenrollment3} className="border rounded-2xl border-black/20 opacity-20" alt=""/>
+            <Image src={reenrollment5} className="border rounded-2xl border-black/20 opacity-20" alt=""/>
+            <Image src={reenrollment2} className="border rounded-2xl border-black/20 opacity-20" alt=""/>
+            <Image src={reenrollment1} className="border rounded-2xl border-black/20 opacity-20" alt=""/>
+            <Image src={reenrollment2} className="border rounded-2xl border-black/20 opacity-20" alt=""/>
 
-            <Image src={reenrollment4} className="border rounded-2xl border-black/20" alt=""/>
-            <Image src={reenrollment3} className="border rounded-2xl border-black/20" alt=""/>
-            <Image src={reenrollment2} className="border rounded-2xl border-black/20" alt=""/>
-            <Image src={reenrollment5} className="border rounded-2xl border-black/20" alt=""/>
-            <Image src={reenrollment1} className="border rounded-2xl border-black/20" alt=""/>
+            <Image src={reenrollment4} className="border rounded-2xl border-black/20 opacity-20" alt=""/>
+            <Image src={reenrollment3} className="border rounded-2xl border-black/20 opacity-20" alt=""/>
+            <Image src={reenrollment2} className="border rounded-2xl border-black/20 opacity-20" alt=""/>
+            <Image src={reenrollment5} className="border rounded-2xl border-black/20 opacity-20" alt=""/>
+            <Image src={reenrollment1} className="border rounded-2xl border-black/20 opacity-20" alt=""/>
         </motion.div>
       </section>
 
@@ -152,7 +138,7 @@ export default function Home() {
             transition={{ delay: 0, duration: .4, ease: "easeOut" }}>
               <h3 className="case-title">Premium posgraduate redesign</h3>
               <p>I redesigned the postgraduate course purchase flow with a premium UI, <span className="underline decoration-2 decoration-yellow-500">achieving 57.14% faster navigation.</span></p>
-              <a href="/posgraduate" className="button-primary bg-yellow-600">View case</a>
+              <a href="/posgraduate" className="button-primary mt-4 bg-yellow-600 border-yellow-600">View case</a>
           </motion.div>
 
           <motion.a          
@@ -191,7 +177,7 @@ export default function Home() {
             transition={{ delay: 0, duration: .4, ease: "easeOut" }}>
               <h3 className="case-title">Payment of outstanding fees</h3>
               <p>How I designed the re-enrollment flow, automating financial processes and <span className="underline decoration-2 decoration-emerald-500">boosting debt clearance.</span></p>
-              <a href="/re-enrollment" className="button-primary bg-emerald-600">View case</a>
+              <a href="/re-enrollment" className="button-primary mt-4 bg-emerald-600 border-emerald-600">View case</a>
           </motion.div>
 
           <motion.a          
@@ -217,8 +203,8 @@ export default function Home() {
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1, y: [20, 0] }}
             transition={{ delay: 0, duration: .4, ease: "easeOut" }}
-            className="font-normal text-2xl text-center">
-              <strong>Tadeu</strong> a well-rounded generalist within the UX Umbrella
+            className="font-normal text-2xl">
+              Tadeu has experience in multiple areas within the UX Umbrella
               <UmbrellaIcon className="ml-2 inline-block"/>
           </motion.h2>
           <div className="grid grid-cols-5 space-x-5">            
@@ -313,19 +299,43 @@ export default function Home() {
             </motion.div>
           </div>
 
-          <motion.div
-            viewport={{ once: true }}
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1, y: [20, 0] }}
-            transition={{ delay: .4, duration: .4, ease: "easeOut" }}
-            className="flex-row space-y-2 text-center">
-              <p className="text-xl">
-                Feel free to reach out, <strong>I look forward to hearing from you</strong>
-              </p>
-              <a href="mailto:tadeugarcia.92@gmail.com" className="btn-link inline-block hover:text-green-700">
-                tadeugarcia.92@gmail.com
-              </a>
-          </motion.div>
+          <div className="flex">
+            <motion.div
+              viewport={{ once: true }}
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1, y: [20, 0] }}
+              transition={{ delay: .4, duration: .4, ease: "easeOut" }}
+              className="w-full flex-row space-y-2">
+                <p className="text-xl">
+                  Feel free to reach out, <strong>I look forward to hearing from you</strong>
+                </p>
+                <a href="mailto:tadeugarcia.92@gmail.com" className="btn-link inline-block hover:text-green-700">
+                  tadeugarcia.92@gmail.com
+                </a>
+            </motion.div>
+
+            <nav className="mx-auto mb-5 w-full inline-flex justify-end text-green-950">          
+              <motion.div
+                viewport={{ once: true }}
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1, y: [20, 0] }}
+                transition={{ delay: .8, duration: .4, ease: "easeOut" }}
+                className="space-x-5">
+                <a href="https://www.linkedin.com/in/garciatadeu/" target="_blank" className="navbar-link">
+                  <Linkedin02Icon/>
+                  LinkedIn
+                </a>
+                <a href="https://github.com/tadeugarcia" target="_blank" className="navbar-link">
+                  <GithubIcon/>
+                  GitHub
+                </a>
+                <a href="Tadeu Garcia - Resume.pdf" target="_blank" className="navbar-link">
+                  <GoogleDocIcon/>
+                  Resume
+                </a>
+              </motion.div>
+            </nav>
+          </div>          
         </div>
       </section>
     </main>
